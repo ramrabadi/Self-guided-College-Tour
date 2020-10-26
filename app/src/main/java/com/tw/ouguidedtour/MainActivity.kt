acb0e56cbc83d42a4b436014b046eace98a840a6
Matthew Ellis <matthewaellis29@gmail.com>
2020-10-26 11:38:47 -0400

    Added check if device supports Wifi Rtt

--- app/src/main/java/com/tw/ouguidedtour/MainActivity.kt
+++ app/src/main/java/com/tw/ouguidedtour/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.tw.ouguidedtour
 
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
+
+import android.Manifest.permission
+import android.content.*
 import android.content.pm.PackageManager
+import android.net.wifi.WifiManager
 import android.net.wifi.rtt.WifiRttManager
 import android.os.Bundle
 import android.widget.Toast
@@ -19,15 +19,35 @@ class MainActivity : AppCompatActivity() {
 
     private val ACCESS_FINE_LOCATION_RQ = 101
     private val ACCESS_CAMERA_RQ = 102
+    private var mLocationPermissionApproved: Boolean = false
+    private var mCameraPermissionApproved: Boolean = false
+    private var mWifiEnabled: Boolean = false
+
+    /** These are for the Checking if Wifi RTT is compatible with the phone **/
+    //private var deviceCompatible: Boolean = false
+    //private lateinit var mWifiRttManager: WifiRttManager
+
+    private lateinit var mWifiManager: WifiManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Timber.i("onCreate Called")
 
+        mWifiManager = getSystemService(Context.WIFI_SERVICE) as WifiManager
 
-        checkForPermissions(android.Manifest.permission.ACCESS_FINE_LOCATION, "Fine Location", ACCESS_FINE_LOCATION_RQ)
-        checkForPermissions(android.Manifest.permission.CAMERA, "Camera", ACCESS_CAMERA_RQ)
+        checkForPermissions(
+            android.Manifest.permission.ACCESS_FINE_LOCATION,
+            "Fine Location",
+            ACCESS_FINE_LOCATION_RQ
+        )
+        checkForPermissions(
+            android.Manifest.permission.CAMERA,
+            "Camera",
+            ACCESS_CAMERA_RQ
+        )
+
+        isDeviceCompatible()
     }
 
     /** Lifecycle Methods **/
@@ -44,6 +64,31 @@ class MainActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
         Timber.i("onResume Called")
+
+        mLocationPermissionApproved =
+            (ActivityCompat.checkSelfPermission(this, permission.ACCESS_FINE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED)
+
+        mCameraPermissionApproved =
+            (ActivityCompat.checkSelfPermission(this, permission.CAMERA)
+                    == PackageManager.PERMISSION_GRANTED)
+
+        /** Checks to see if wifi is currently enabled and if wifi is not enabled then
+         *   ask the user to enable wifi.  .setWifiEnabled() was deprecated in API 29**/
+        mWifiEnabled = mWifiManager.isWifiEnabled
+        Timber.i("mWifiEnabled: $mWifiEnabled")
+
+        if (!mWifiEnabled) {
+            val builder = AlertDialog.Builder(this)
+            builder.apply {
+                setMessage("You do not have Wifi Enabled right now")
+                setTitle("Wifi Required")
+                setPositiveButton("OK") { dialogInterface: DialogInterface, i: Int -> }
+            }
+            val dialog = builder.create()
+            dialog.show()
+        }
+
 
         // Resume playing video
     }
@@ -97,7 +142,11 @@ class MainActivity : AppCompatActivity() {
             ContextCompat.checkSelfPermission(applicationContext, permission) == PackageManager.PERMISSION_GRANTED -> {
                 Toast.makeText(applicationContext, "$name permission granted", Toast.LENGTH_SHORT).show()
             }
-            shouldShowRequestPermissionRationale(permission) -> showWhyRequest(permission, name, requestCode)
+            shouldShowRequestPermissionRationale(permission) -> showWhyRequest(
+                permission,
+                name,
+                requestCode
+            )
 
             else -> ActivityCompat.requestPermissions(this, arrayOf(permission), requestCode)
         }
@@ -129,7 +178,11 @@ class MainActivity : AppCompatActivity() {
             setMessage("Permission to access your $name is required help navigate you to the Stocker and through Stocker.")
             setTitle("Permission Required")
             setPositiveButton("OK") { dialog, which ->
-                ActivityCompat.requestPermissions(this@MainActivity, arrayOf(permission), requestCode)
+                ActivityCompat.requestPermissions(
+                    this@MainActivity,
+                    arrayOf(permission),
+                    requestCode
+                )
             }
         }
 
@@ -169,6 +222,49 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    /**Checks if Wifi Rtt is supported
+     *
+     *  This isn't required for use, but is needed for error handling.
+     *
+     * **/
+
+    private fun isDeviceCompatible(){
+        /** Error this always returns false **/
+        val isCompatible = this.packageManager.hasSystemFeature(PackageManager.FEATURE_WIFI_RTT)
+
+        Timber.i("isCompatible: $isCompatible")
+//        if(isCompatible){
+//            Timber.i("Device is compatible")
+//            Toast.makeText(applicationContext, "Wifi Rtt is Supported", Toast.LENGTH_SHORT).show()
+//            //deviceCompatible = true
+//
+//        }else{
+//            val wifiRttManager: WifiRttManager =getSystemService(Context.WIFI_RTT_RANGING_SERVICE) as WifiRttManager
+//            val myReceiver = object : BroadcastReceiver(){
+//                override fun onReceive(context: Context, intent: Intent){
+//                    if(wifiRttManager.isAvailable){
+//                        Timber.i("Device is compatible")
+//                        Toast.makeText(
+//                            applicationContext,
+//                            "Wifi Rtt is Supported",
+//                            Toast.LENGTH_SHORT
+//                        ).show()
+//                    }else{
+//                        Timber.i("Device is not compatible")
+//                        Toast.makeText(
+//                            applicationContext,
+//                            "Wifi Rtt is not Supported",
+//                            Toast.LENGTH_SHORT
+//                        ).show()
+//                    }
+//                }
+//            }
+//            registerReceiver(
+//                myReceiver,
+//                IntentFilter(WifiRttManager.ACTION_WIFI_RTT_STATE_CHANGED)
+//            )
+//        }
+    }
 
 }
 
