734f23e936ebf5a094f346eb4ffc3a75a3ec8393
PC <rameziscool@gmail.com>
2020-10-26 17:09:43 -0400

    Splash Screen and Button to Open Camera.

--- app/src/main/java/com/tw/ouguidedtour/MainActivity.kt
+++ app/src/main/java/com/tw/ouguidedtour/MainActivity.kt
@@ -1,10 +1,18 @@
 package com.tw.ouguidedtour
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
+    private val cameraRequest = 1555
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -13,7 +21,17 @@ class MainActivity : AppCompatActivity() {
         // Check for wifi permissions
         // Check for gps permissions
 
-
+        // Get Camera Permissions
+        if (ContextCompat.checkSelfPermission(applicationContext, Manifest.permission.CAMERA)
+            == PackageManager.PERMISSION_DENIED)
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), cameraRequest)
+        // Initialize button
+        val scanQRCode: Button = findViewById(R.id.button)
+        // Open Camera
+        scanQRCode.setOnClickListener {
+            val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(cameraIntent, cameraRequest)
+        }
     }
 
     /** Lifecycle Methods **/
---
+++ app/src/main/java/com/tw/ouguidedtour/SplashScreen.java
@@ -0,0 +1,27 @@
+package com.tw.ouguidedtour;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                // Showing splash screen with a timer.
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 2000);
+    }
+}
+
