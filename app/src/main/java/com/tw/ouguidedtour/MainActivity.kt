99a5e2afdc42ea49a23fd25f83a3a173b2e7f7a6
Joe Mark Schumacher <34613608+joeyschu1414@users.noreply.github.com>
2020-11-30 18:56:16 -0500

    Sprint3 Changes
    added a  main menu actvity that has one button to scan a QR code and another to see the map view

--- app/src/main/java/com/tw/ouguidedtour/MainActivity.kt
+++ app/src/main/java/com/tw/ouguidedtour/MainActivity.kt
@@ -129,41 +129,14 @@ class MainActivity : AppCompatActivity() {
             val below28API = Intent(this, FloorPlan::class.java)
             startActivity(below28API)
         }
-        val scanQRCode: Button = findViewById(R.id.QRCodeButton)
-        // Open Camera
-        scanQRCode.setOnClickListener {
-            var cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            startActivityForResult(cameraIntent, ACCESS_CAMERA_RQ)
-            val intentIntegrator = IntentIntegrator(this@MainActivity)
-            intentIntegrator.setBeepEnabled(false)
-            intentIntegrator.setCameraId(0)
-            intentIntegrator.setPrompt("SCAN")
-            intentIntegrator.setBarcodeImageEnabled(false)
-            intentIntegrator.initiateScan()
+        val mainMenuButton: Button = findViewById(R.id.MainMenuButton)
+        mainMenuButton.setOnClickListener {
+            val dataIntent = Intent(this, MainMenuActivity::class.java)
+            startActivity(dataIntent)
 
-
-        }
     }
 
-    //Sends QR data to Database activity
-    override fun onActivityResult(
-        requestCode: Int,
-        resultCode: Int,
-        data: Intent?
-    ) {
-        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
-        if (result != null) {
-            if (result.contents == null) {
-                Toast.makeText(this, "cancelled", Toast.LENGTH_SHORT).show()
-            } else {
-                val dataIntent = Intent(this, DataActivity::class.java)
-                dataIntent.putExtra("QRData", result.contents)
-                startActivity(dataIntent)
-            }
-        } else {
-            super.onActivityResult(requestCode, resultCode, data)
-        }
-    }
+
 
 
     /** Lifecycle Methods **/
