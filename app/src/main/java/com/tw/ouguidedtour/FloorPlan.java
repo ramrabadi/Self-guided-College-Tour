1d46a5f88461f50caa9058bb3765b426ea7e5d3f
PC <rameziscool@gmail.com>
2020-11-30 20:07:16 -0500

    - Portrait / Landscape orientation - Update Floor Plan Images - Database configuration file

--- app/src/main/java/com/tw/ouguidedtour/FloorPlan.java
+++ app/src/main/java/com/tw/ouguidedtour/FloorPlan.java
@@ -1,6 +1,11 @@
 package com.tw.ouguidedtour;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FloorPlan extends AppCompatActivity {
@@ -11,5 +16,38 @@ public class FloorPlan extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_floor_plan);
+        Button floor_one = (Button) findViewById(R.id.fp_btn_one);
+        Button floor_two = (Button) findViewById(R.id.fp_btn_two);
+        Button floor_three = (Button) findViewById(R.id.fp_btn_three);
+
+        floor_one.setOnClickListener(v -> openFirstFloor());
+
+        floor_two.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSecondFloor();
+            }
+        });
+
+        floor_three.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openThirdFloor();
+            }
+        });
+    }
+    public void openFirstFloor() {
+        Intent intent = new Intent(this, FloorOne.class);
+        startActivity(intent);
+    }
+
+    public void openSecondFloor() {
+        Intent intent = new Intent(this, FloorTwo.class);
+        startActivity(intent);
+    }
+
+    public void openThirdFloor() {
+        Intent intent = new Intent(this, FloorThree.class);
+        startActivity(intent);
     }
 }
