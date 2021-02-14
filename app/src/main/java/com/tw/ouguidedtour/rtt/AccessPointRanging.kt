8757393b5d5f29fe0c57fdcee4faf66f45e097ce
Matthew Ellis <matthewaellis29@gmail.com>
2021-02-14 16:16:37 -0500

    Finished RTT Ranging, change type for distance in RttNode

--- app/src/main/java/com/tw/ouguidedtour/rtt/AccessPointRanging.kt
+++ app/src/main/java/com/tw/ouguidedtour/rtt/AccessPointRanging.kt
@@ -1,7 +1,6 @@
 package com.tw.ouguidedtour.rtt
 
 import android.Manifest.permission
-import android.content.Context
 import android.content.pm.PackageManager
 import android.net.wifi.ScanResult
 import android.net.wifi.rtt.RangingRequest
@@ -12,116 +11,146 @@ import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import com.tw.ouguidedtour.fspl.AccessPointData
+import timber.log.Timber
+import java.util.*
 
 
-class AccessPointRanging: AppCompatActivity() {
+class AccessPointRangingResultsActivity : AppCompatActivity() {
 
-    private val SCAN_RESULT_EXTRA: String = "com.tw.ouguidedtour.extra.SCAN_RESULT"
+    val SCAN_RESULT_EXTRA: String = "com.tw.ouguidedtour.rtt.extra.SCAN_RESULT"
 
-    private lateinit var mScanResult: ScanResult
-
-    private lateinit var mMac: String
-
+    lateinit var data: MutableList<AccessPointRTTData>
+    private var mScanResult: ScanResult? = null
+    private var mBssid: String? = null
+    private var mDistance: Float = 0.0f
     private var mNumberOfRangeRequests = 0
     private var mNumberOfSuccessfulRangeRequests = 0
+    private var mMillisecondsDelayBeforeNewRangingRequest = 0
 
-    private var mSampleSize = 0
+    private var mSampleSize = 1
 
-    private var mStatisticRangeHistoryEndIndex = 0
-    private var mStatisticRangeHistory: ArrayList<Int>? = null
+    private var mDistanceRangeHistoryEndIndex = 0
+    private var mDistanceRangeHistory: ArrayList<Int>? = null
+    private var mLastMeanDistance = 0
 
     private var mWifiRttManager: WifiRttManager? = null
     private var mRttRangingResultCallback: RttRangingResultCallback? = null
 
-
-    private var mTimeBeforeNewRangingRequest: Int = 0
-
-    val mRangeRequestDelayHandler: Handler = Handler()
-
+    val mRangeRequestDelayHandler = Handler()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val intent = intent
-        mScanResult = intent.getParcelableExtra(SCAN_RESULT_EXTRA)!!
-
+        mScanResult = intent.getParcelableExtra(SCAN_RESULT_EXTRA)
         if (mScanResult == null) {
             finish()
         }
 
-        // Stores the mac address of th access point (i.e. The wifi router)
-        mMac = mScanResult.BSSID
-
-        mWifiRttManager = getSystemService(Context.WIFI_RTT_RANGING_SERVICE) as WifiRttManager
+        mBssid = mScanResult!!.BSSID
+        mWifiRttManager = getSystemService(WIFI_RTT_RANGING_SERVICE) as WifiRttManager
         mRttRangingResultCallback = RttRangingResultCallback()
 
-        mStatisticRangeHistory = ArrayList()
 
+        mDistanceRangeHistory = ArrayList()
+        resetData()
         startRangingRequest()
-
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        remove_history()
+    private fun resetData() {
+        mSampleSize = 0
+        mMillisecondsDelayBeforeNewRangingRequest = 0
+        mNumberOfSuccessfulRangeRequests = 0
+        mNumberOfRangeRequests = 0
+        mDistanceRangeHistoryEndIndex = 0
+        mDistanceRangeHistory!!.clear()
     }
 
     private fun startRangingRequest() {
-
         if (ActivityCompat.checkSelfPermission(this, permission.ACCESS_FINE_LOCATION)
             != PackageManager.PERMISSION_GRANTED
         ) {
             finish()
         }
         mNumberOfRangeRequests++
-        val rangingRequest = RangingRequest.Builder().addAccessPoint(mScanResult).build()
-        mRttRangingResultCallback?.let {
-            mWifiRttManager!!.startRanging(
-                rangingRequest, application.mainExecutor, it
-            )
-        }
+        val rangingRequest = RangingRequest.Builder().addAccessPoint(mScanResult!!).build()
+        mWifiRttManager!!.startRanging(
+            rangingRequest, application.mainExecutor, mRttRangingResultCallback!!
+        )
     }
 
-    // Calculates the mean distance I don't know if we will need or use this but it could be useful
-    private fun getMeanDistance(): Float {
-        var distanceSum = 0f
-        for (distance in mStatisticRangeHistory!!) {
-            distanceSum += distance.toFloat()
-        }
-        // !!.size is for if mStatisicRangeHistory == 0
-        return distanceSum / mStatisticRangeHistory!!.size
-    }
-
-    // Adds the distance to that access points history, we can use this to tell if the user is moving or not with the getMeanDistance
-    private fun addDistanceToHistory(distance: Int) {
-        if (mStatisticRangeHistory!!.size >= mSampleSize) {
-            if (mStatisticRangeHistoryEndIndex >= mSampleSize) {
-                mStatisticRangeHistoryEndIndex = 0
+    private val meanDistance: Float
+        get() {
+            var distanceSum = 0f
+            for (distance in mDistanceRangeHistory!!) {
+                distanceSum += distance.toFloat()
             }
-            mStatisticRangeHistory!![mStatisticRangeHistoryEndIndex] = distance
-            mStatisticRangeHistoryEndIndex++
+            return distanceSum / mDistanceRangeHistory!!.size
+        }
+
+    private fun addDistanceToHistory(distance: Int) {
+        if (mDistanceRangeHistory!!.size >= mSampleSize) {
+            if (mDistanceRangeHistoryEndIndex >= mSampleSize) {
+                mDistanceRangeHistoryEndIndex = 0
+            }
+            mDistanceRangeHistory!![mDistanceRangeHistoryEndIndex] = distance
+            mDistanceRangeHistoryEndIndex++
         } else {
-            mStatisticRangeHistory!!.add(distance)
+            mDistanceRangeHistory!!.add(distance)
         }
     }
 
-    private fun remove_history () {
-        mSampleSize = 0
-        mNumberOfSuccessfulRangeRequests = 0
-        mNumberOfRangeRequests = 0
-        mStatisticRangeHistoryEndIndex = 0
-        mStatisticRangeHistory!!.clear()
-    }
-    // TODO
-    private class RttRangingResultCallback() : RangingResultCallback() {
+    // Class that handles callbacks for all RangingRequests and issues new RangingRequests.
+    private inner class RttRangingResultCallback : RangingResultCallback() {
         private fun queueNextRangingRequest() {
+            mRangeRequestDelayHandler.postDelayed(
+                { startRangingRequest() },
+                mMillisecondsDelayBeforeNewRangingRequest.toLong()
+            )
         }
 
         override fun onRangingFailure(code: Int) {
-
+            Timber.i(
+                "onRangingFailure() code: $code"
+            )
+            queueNextRangingRequest()
         }
 
         override fun onRangingResults(list: List<RangingResult>) {
+            Timber.i(
+                "onRangingResults(): $list"
+            )
 
+            for(item in list) {
+                if (item.status == RangingResult.STATUS_SUCCESS) {
+                    mNumberOfSuccessfulRangeRequests++
+                    mDistance = (item.distanceMm / 1000).toFloat()
+                    val temp = meanDistance
+                    addDistanceToHistory(item.distanceMm)
+
+                    convertToObject(item.macAddress.toString(), mDistance, temp)
+                } else if (item.status
+                    == RangingResult.STATUS_RESPONDER_DOES_NOT_SUPPORT_IEEE80211MC
+                ) {
+                    Timber.i("RangingResult failed (AP doesn't support IEEE80211 MC.")
+                } else {
+                    Timber.i("RangingResult failed.")
+                }
+
+            }
+
+            queueNextRangingRequest()
         }
     }
+
+
+    private fun convertToObject(bssi: String, distance: Float, lastMean: Float) {
+        lateinit var temp: AccessPointRTTData
+        temp.BSSID = bssi
+        temp.DISTANCE = distance
+        temp.LASTMEAN = lastMean
+
+        data.add(temp)
+    }
 }
+
