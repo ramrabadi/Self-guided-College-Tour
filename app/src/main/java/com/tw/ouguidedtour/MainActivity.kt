7451d2bd529dcdeabbe7d4637fcf193884e85134
Matthew Ellis <matthewaellis29@gmail.com>
2021-04-13 16:53:19 -0400

    Moved permissions check to MainMenu from Main
    Also changed the message the user see's when we are requesting permission to be a bit more clear about why we are requesting the use of the camera and their location.

--- app/src/main/java/com/tw/ouguidedtour/MainActivity.kt
+++ app/src/main/java/com/tw/ouguidedtour/MainActivity.kt
@@ -57,20 +57,17 @@ import java.io.InputStream
 class MainActivity : AppCompatActivity() {
 
     private var locationManager: LocationManager? = null
-    private val ACCESS_FINE_LOCATION_RQ = 101
-    private val ACCESS_CAMERA_RQ = 102
     private var mLocationPermissionApproved: Boolean = false
     private var mCameraPermissionApproved: Boolean = false
     private var mWifiEnabled: Boolean = false
     private var map: MapView? = null
-    val x: Int = 1235
 
     var endpoint: GeoPoint = GeoPoint(39.3262283, -82.1068219)
 
     var currentFloor = 1
     var endFloor = 3
     var displayFloor = 1
-    val gpsRoadManager: RoadManager = GraphHopperRoadManager("b48048f0-1ee2-4459-ad43-9e5da2d005eb", false)
+    val gpsRoadManager: RoadManager = GraphHopperRoadManager(R.string.GraphHopperAPI.toString(), false)
     var hopper1 = GraphHopperOSM()
     var hopper2 = GraphHopperOSM()//graph data and routing for each floor
     var hopper3 = GraphHopperOSM()
@@ -251,10 +248,8 @@ class MainActivity : AppCompatActivity() {
         //map!!.setTileSource(TileSourceFactory.MAPNIK)
         map!!.setMultiTouchControls(true)
 
-      
         val destFloor = 3
         val destLoc = GeoPoint(39.3261779, -82.106899)
-
 
         /** Draw image of Storcker on map */
         myGroundOverlay.position = GeoPoint(39.3261511, -82.1069252)
@@ -402,17 +397,6 @@ class MainActivity : AppCompatActivity() {
         Configuration.getInstance()
                 .load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx))
 
-        checkForPermissions(
-            android.Manifest.permission.ACCESS_FINE_LOCATION,
-            "Fine Location",
-            ACCESS_FINE_LOCATION_RQ
-        )
-        checkForPermissions(
-            android.Manifest.permission.CAMERA,
-            "Camera",
-            ACCESS_CAMERA_RQ
-        )
-
     }
 
     // Sets QR code value for use in TourActivity
@@ -431,9 +415,8 @@ class MainActivity : AppCompatActivity() {
                 val tourIntent = Intent(this, TourActivity::class.java)
 
                 if (tour.getId() == "None") {
-                    // TODO Change Test.json to config file variable
-                    tour.setId(tour.get_tour_id(qrString,"Test.json", assets))
-                    tour.load_list_of_stops(tour, qrString, "Test.json", assets)
+                    tour.setId(tour.get_tour_id(qrString,R.string.JSONFile.toString(), assets))
+                    tour.load_list_of_stops(tour, qrString, R.string.JSONFile.toString(), assets)
 
                     currentLocation = tour.getLocation(tour, qrString)
                     nextLocation = tour.getLocation(tour, currentLocation.getNextLocationId())
@@ -511,11 +494,10 @@ class MainActivity : AppCompatActivity() {
         mWifiEnabled = mWifiManager.isWifiEnabled
         Timber.i("mWifiEnabled: $mWifiEnabled")
 
-        //TODO Tell the user why we need this, to load the maps
         if (!mWifiEnabled) {
             val builder = AlertDialog.Builder(this)
             builder.apply {
-                setMessage("You do not have Wifi Enabled right now")
+                setMessage("You do not have Wifi Enabled right now. Please enable wifi so that the app can help you navigate around Stocker.")
                 setTitle("Wifi Required")
                 setPositiveButton("OK") { dialogInterface: DialogInterface, i: Int -> }
             }
@@ -709,115 +691,7 @@ class MainActivity : AppCompatActivity() {
         //updateDestination(tempNavigationData.getLat())
     }
 
-    /** Functions which check for permission and request permissions **/
 
-    /**
-     *  Function: checkForPermissions
-     *
-     *  Purpose: This checks if the given permission has been granted or not, if it has been granted it displays a Toast to the screen
-     *           sawing that the permission has been granted if not then it calls the requestPermissions function from ActivityCompat,
-     *           this is the function that will be called to check and if not already granted calls other functions to request the permissions.
-     *
-     *  Parameters:     permission: String, Exp(android.Manifest.permission.ACCESS_FINE_LOCATION) This is a permission which must be in the Manifest.xml file.
-     *
-     *                  name: String, This is a string value that is a name given to the Permission by the coder and will be used if the permission has not
-     *                                  been given already.
-     *
-     *                  requestCode: Int, This is a integer value that is created by the coder it could be any value only used later to check which permission needs
-     *                                     to be used to request the permission
-     *
-     *  Pre-condition:  Valid input
-     *
-     *  Post-condition: If the permission being checked is granted sends a toast to the screen. If not calls showWhyRequest which displays a prompt for why we are asking
-     *                  for this permission to be granted.
-     *
-     */
-    private fun checkForPermissions(permission: String, name: String, requestCode: Int) {
-        when {
-            ContextCompat.checkSelfPermission(applicationContext, permission) == PackageManager.PERMISSION_GRANTED -> {
-                Toast.makeText(applicationContext, "$name permission granted", Toast.LENGTH_SHORT).show()
-            }
-            shouldShowRequestPermissionRationale(permission) -> showWhyRequest(
-                permission,
-                name,
-                requestCode
-            )
-
-            else -> ActivityCompat.requestPermissions(this, arrayOf(permission), requestCode)
-        }
-    }
-
-
-    /**
-     *  Function: showWhyRequest
-     *
-     *  Purpose: Shows a dialog box in the middle of the screen telling the user why we are going to request location permissions
-     *
-     *  Parameters: permission: String, Exp(android.Manifest.permission.ACCESS_FINE_LOCATION) This is a permission which must be in the Manifest.xml file.
-     *
-     *              name: String, This is a string value that is a name given to the Permission by the coder and will be used if the permission has not
-     *                            been given already.
-     *
-     *             requestCode: Int, This is a integer value that is created by the coder it could be any value only used later to check which permission needs
-     *                               to be used to request the permission
-     *
-     *  Pre-condition: Valid input
-     *
-     *  Post-condition: Displays dialog box and then once the user hit the OK button then stack goes back to checkForPermissions and to the else statement
-     *
-     */
-    private fun showWhyRequest(permission: String, name: String, requestCode: Int) {
-        val builder = AlertDialog.Builder(this)
-
-        builder.apply {
-            setMessage("Permission to access your $name is required help navigate you to the Stocker and through Stocker.")
-            setTitle("Permission Required")
-            setPositiveButton("OK") { dialog, which ->
-                ActivityCompat.requestPermissions(
-                    this@MainActivity,
-                    arrayOf(permission),
-                    requestCode
-                )
-            }
-        }
-
-        val dialog = builder.create()
-
-        dialog.show()
-    }
-
-    /**
-     *  Function: override of onRequestPermissionResult
-     *
-     *  Purpose: Shows a dialog box in the middle of the screen telling the user why we are going to request location permissions
-     *
-     *  Parameters: context: this@MainActivity
-     *
-     *              permission: String, Exp(android.Manifest.permission.ACCESS_FINE_LOCATION) This is a permission which must be in the Manifest.xml file
-     *
-     *             grandResults: IntArray,
-     *
-     */
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        fun innerCheck(name: String) {
-            if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(applicationContext, "$name permission denied", Toast.LENGTH_SHORT).show()
-            } else {
-                Toast.makeText(applicationContext, "$name permission granted", Toast.LENGTH_SHORT).show()
-            }
-        }
-
-        when (requestCode) {
-            ACCESS_FINE_LOCATION_RQ -> innerCheck("Fine Location")
-            ACCESS_CAMERA_RQ -> innerCheck("Camera")
-        }
-    }
-
-    /** ----------------------------------------------------------------------- **/
 
     /**
      * Checks if location is within Stocker
